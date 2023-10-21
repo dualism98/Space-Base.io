d01949b40ee137ef4a226351b7c9b9402598ccf3
dualism <beachsidebearhi@gmail.com>
2023-10-21 17:13:05 +0300

    Changed server port

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 var app = express();
 
 //Initalizing a server on port 80 using the ipv4 address of the machine it is running off of
-var server = app.listen(8080, "0.0.0.0");
+var server = app.listen(80, "0.0.0.0");
 app.use(express.static(path.join(__dirname, '../client')));
 var io = SocketIO.listen(server);   //socket(server);
 
